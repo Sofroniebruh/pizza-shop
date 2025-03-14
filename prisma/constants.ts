15f652b04e680c73d7d3ea669cdd4fee03dda454
Sofronie Albu <esnu@MacBook-Pro-Sofronie.local>
2025-03-14 11:09:09 +0100

    progress to seed file

--- prisma/constants.ts
+++ prisma/constants.ts
@@ -103,3 +103,91 @@ export const _ingredients = [
         imageUrl: '/meatballs.png',
     },
 ].map((obj, index) => ({id: index + 1, ...obj}));
+
+export const products = [
+    {
+        name: 'Omelet with Ham and Mushrooms',
+        imageUrl: '/shawarma1.png',
+        categoryId: 2,
+    },
+    {
+        name: 'Omelet with Pepperoni',
+        imageUrl: '/shawarma2.png',
+        categoryId: 2,
+    },
+    {
+        name: 'Coffee Latte',
+        imageUrl: '/coffee_latte.png',
+        categoryId: 2,
+    },
+    {
+        name: 'Sandwich Ham and Cheese',
+        imageUrl: '/sandwich.webp',
+        categoryId: 3,
+    },
+    {
+        name: 'Chicken Nuggets',
+        imageUrl: '/nuggets.png',
+        categoryId: 3,
+    },
+    {
+        name: 'Baked Potatoes with Sauce',
+        imageUrl: '/potatoes.webp',
+        categoryId: 3,
+    },
+    {
+        name: 'Shawarma',
+        imageUrl: '/shawarma.webp',
+        categoryId: 3,
+    },
+    {
+        name: 'Spicy Shawarma',
+        imageUrl: '/spicy_shawarma.webp',
+        categoryId: 3,
+    },
+    {
+        name: 'Banana Milkshake',
+        imageUrl: '/milkshake.png',
+        categoryId: 4,
+    },
+    {
+        name: 'Caramel Apple Milkshake',
+        imageUrl: '/caramel_milkshake.png',
+        categoryId: 4,
+    },
+    {
+        name: 'Milkshake with Oreo Cookies',
+        imageUrl: '/oreo_milkshake.png',
+        categoryId: 4,
+    },
+    {
+        name: 'Classic Milkshake',
+        imageUrl: 'classic_milkshake.png',
+        categoryId: 4,
+    },
+    {
+        name: 'Irish Cappuccino',
+        imageUrl: '/irish_cappuccino.png',
+        categoryId: 5,
+    },
+    {
+        name: 'Caramel Cappuccino',
+        imageUrl: '/caramel_cappuccino.webp',
+        categoryId: 5,
+    },
+    {
+        name: 'Coconut Latte',
+        imageUrl: '/coconut_latte.webp',
+        categoryId: 5,
+    },
+    {
+        name: 'Americano Coffee',
+        imageUrl: '/americano_coffee.webp',
+        categoryId: 5,
+    },
+    {
+        name: 'Coffee Latte',
+        imageUrl: '/coffee_latte.webp',
+        categoryId: 5,
+    },
+];
