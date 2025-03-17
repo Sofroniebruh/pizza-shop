866e0db5cbd8f735114c2e37a757d8127bbc95d0
Sofronie Albu <esnu@MacBook-Pro-Sofronie.local>
2025-03-17 20:35:39 +0100

    added price range functionality

--- components/shared-components/filtering.tsx
+++ components/shared-components/filtering.tsx
@@ -10,6 +10,11 @@ import {API} from "@/lib/services/api_client";
 import {toast} from "sonner";
 import {useSet} from "react-use";
 
+interface PriceProps {
+    priceFrom: number,
+    priceTo: number,
+}
+
 export default function Filtering({className}: { className?: string }) {
     const [ingredients, setIngredients] = useState<Ingredient[]>([]);
     const [isLoading, setIsLoading] = useState(false);
@@ -18,6 +23,14 @@ export default function Filtering({className}: { className?: string }) {
         text: ingredient.name
     }));
     const [selectedIds, {toggle}] = useSet(new Set<string>([]))
+    const [prices, setPrices] = useState<PriceProps>({priceFrom: 0, priceTo: 30});
+
+    const handlePriceChange = (name: keyof PriceProps, amount: number) => {
+        setPrices({
+            ...prices,
+            [name]: amount
+        })
+    }
 
     useEffect(() => {
         setIsLoading(true)
@@ -40,11 +53,17 @@ export default function Filtering({className}: { className?: string }) {
             <div className={"border-t border-gray-200 mt-7 py-4"}>
                 <p className={"mb-2 font-semibold"}>Price range:</p>
                 <div className={"flex gap-3"}>
-                    <Input type={"number"} placeholder={"0"} min={0} max={30} defaultValue={0}></Input>
-                    <Input type={"number"} min={0} max={30} placeholder={"30"}></Input>
+                    <Input type={"number"} placeholder={"0"} min={0} max={30} defaultValue={prices.priceFrom}
+                           onChange={(e) => handlePriceChange("priceFrom", Number(e.target.value))}></Input>
+                    <Input type={"number"} min={0} max={30} defaultValue={prices.priceTo}
+                           onChange={(e) => handlePriceChange("priceTo", Number(e.target.value))}></Input>
                 </div>
                 <div className={"mt-4"}>
-                    <RangeSlider min={0} max={30} step={1} value={[0, 30]}></RangeSlider>
+                    <RangeSlider min={0} max={30} step={1} value={[prices.priceFrom, prices.priceTo]}
+                                 onValueChange={([from, to]) => setPrices({
+                                     priceFrom: from,
+                                     priceTo: to
+                                 })}></RangeSlider>
                 </div>
                 <FilterCheckboxGroup
                     className={"mt-8"}
